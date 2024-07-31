0b6aa5a9e1f4eed834eaed8c1dda21160b25886c
张竞豪 <zhangjinghao@insta360.com>
2024-07-31 19:58:22 +0800

    This is the 7th commmit on master

--- hello_world.py
+++ hello_world.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     print("Hello World again on master")
     print("Hello it's me again on master")
     print("This is master branch")
+    print("this is master branch 12345")
     dev_print()
     print("This is master branch after 1st merge")
     print("Hello 12345")
