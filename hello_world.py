4f55dd340e63e9b4e30d6d0c10f97a5838daaf62
张竞豪 <zhangjinghao@insta360.com>
2024-07-31 19:37:40 +0800

    This is the 6th commmit on master

--- hello_world.py
+++ hello_world.py
@@ -6,3 +6,5 @@ if __name__ == "__main__":
     print("Hello it's me again on master")
     print("This is master branch")
     dev_print()
+    print("This is master branch after 1st merge")
+    print("Hello 12345")
